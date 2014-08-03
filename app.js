a1285fa6e267e8e59400036f85ad512e8c8c746d
Vinay <vbvyas@gmail.com>
2014-08-03 02:21:53 -0700

    Complete Bart prototype api.

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 var Hapi = require('hapi');
+var geolib = require('geolib');
+var request = require('request');
+var parser = require('xml2js').parseString;
+var stations = require('./stations.json');
+var fake = require('./fake.json');
+
+var bart_key = process.env.BART || "MW9S-E7SL-26DU-VV8V";
 var app = Hapi.createServer('0.0.0.0', parseInt(process.env.PORT, 10) || 3000);
+var url = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig="
+var key = "&key=" + bart_key;
 
 // routes
 app.route({
   method: 'GET',
-  path: '/',
+  path: '/station/{stn}',
   handler: function (req, res) {
-    res('app is running');
+    var stn = req.params.stn;
+    if (stn === 'FAKE' || stn === 'fake') {
+      res(fake);
+    } else {
+      var queryUrl = url + stn + key;
+      request(queryUrl, function(err, request_res, body) {
+        if (err) {
+          res([]);
+        } else {
+          parser(body, function (err, result) {
+            res(result);
+          });
+        }
+      });
+    }
+  }
+});
+
+app.route({
+  method: 'GET',
+  path: '/stations',
+  handler: function (req, res) {
+    var coordinate = {};
+    coordinate.latitude = req.query.lat;
+    coordinate.longitude = req.query.lng;
+    var sorted = geolib.orderByDistance(coordinate, stations);
+    var closest = [];
+    for (var i = 0; i < sorted.length; i++) {
+      var s = sorted[i];
+      var station = stations[s.key];
+      var point = {};
+      point.name = station.name;
+      point.abbr = station.abbr;
+      point.distance = geolib.convertUnit('mi', s.distance); // miles
+      closest.push(point);
+    }
+    res(closest);
   }
 });
 
